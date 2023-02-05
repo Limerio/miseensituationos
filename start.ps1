b5651a6a99ce291d3d3c7e77044c2b1251c7786d
Limerio <44976231+Limerio@users.noreply.github.com>
2023-02-05 16:15:05 +0100

    Moove script deploy_forest to start script

--- start.ps1
+++ start.ps1
@@ -2,3 +2,19 @@ New-NetIPAddress -InterfaceIndex 3 -IPAddress 192.168.0.1 -PrefixLength 24
 Install-WindowsFeature DNS
 Install-WindowsFeature DHCP
 Add-WindowsFeature AD-Domain-Services
+
+Import-Module ADDSDeployment
+
+Install-ADDSForest `
+-CreateDnsDelegation:$false `
+-DatabasePath "C:\Windows\NTDS" `
+-DomainMode "WinThreshold" `
+-DomainName "marvelle.local" `
+-DomainNetbiosName "MARVELLE" `
+-ForestMode "WinThreshold" `
+-InstallDns:$true `
+-LogPath "C:\Windows\NTDS" `
+-NoRebootOnCompletion:$false `
+-SysvolPath "C:\Windows\SYSVOL" `
+-Force:$true
+
