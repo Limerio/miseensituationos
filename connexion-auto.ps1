7b049f9c0c1a774e026eccd8499135361d8018c3
WookiesRpeople2 <117142278+WookiesRpeople2@users.noreply.github.com>
2023-02-05 23:10:36 +0100

    Update connexion-auto.ps1

--- connexion-auto.ps1
+++ connexion-auto.ps1
@@ -1,5 +1,4 @@
 netsh interface ipv4 set address "connexion au réseau local" dhcp
 netsh interface ipv4 set dnsserver "connexion au réseau local" dhcp
-Rename-Computer -NewName "CPCO" 
-Add-Computer -DomainName "marvelle.local" -Credential "Administrateur"
+Add-Computer -DomainName "marvelle.local"
 Restart-Computer
