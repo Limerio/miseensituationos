e84e6b50a0bec25e1e3cdd4306b5b64cc2b078bc
Keenan <keenantobiansky@gmail.com>
2023-02-07 17:31:12 +0100

    dit moi ce que t'en pense

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 //put js code
+
+const observe = new IntersectionObserver((el) => {
+  el.forEach((element) => {
+    if (element.isIntersecting) {
+      element.target.classList.add("open");
+    } else {
+      element.target.classList.remove("open");
+    }
+  });
+});
+
+const all_text_sections = document.querySelectorAll(".not-open");
+
+all_text_sections.forEach((element) => observe.observe(element));
