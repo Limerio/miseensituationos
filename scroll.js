15c135c1f1b64eb3f0a57ad7dadd4d547ea69e96
Keenan <keenantobiansky@gmail.com>
2023-02-10 09:09:41 +0100

    updated photo

--- scroll.js
+++ scroll.js
@@ -1,5 +1,15 @@
 //put js code
 
+var load = document.querySelector(".preloader");
+
+window.addEventListener("load", () => {
+  load.style.display = "none";
+});
+
+/*
+--------------------------------------------------------------------------------------------
+*/
+
 const observe = new IntersectionObserver((el) => {
   el.forEach((element) => {
     if (element.isIntersecting) {
@@ -13,6 +23,23 @@ const observe = new IntersectionObserver((el) => {
 const all_text_sections = document.querySelectorAll(".not-open");
 
 all_text_sections.forEach((element) => observe.observe(element));
+
+/*
+--------------------------------------------------------------------------------------------
+*/
+
+const text = document.querySelector("#text-change");
+var names = ["keenan Tobiansky", "Vincent Raoul"];
+var count = 0;
+function change() {
+  text.innerHTML = names[count];
+  console.log(names[count]);
+  count++;
+  if (count >= names.length) {
+    count = 0;
+  }
+}
+setInterval(change, 2000);
 
 /*
 --------------------------------------------------------------------------------------------
