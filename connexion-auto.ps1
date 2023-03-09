77acabb744c7d3e9d04f5f8dd3fbb9724183ad3f
WookiesRpeople2 <117142278+WookiesRpeople2@users.noreply.github.com>
2023-03-09 17:05:44 +0100

    Update connexion-auto.ps1

--- connexion-auto.ps1
+++ connexion-auto.ps1
@@ -1,4 +1,8 @@
+$Error.Clear()
 netsh interface ipv4 set address "ethernet" dhcp
 netsh interface ipv4 set dnsserver "ethernet" dhcp
+Rename-Computer -NewName "CPCO1"
+if(!$Error){
 Add-Computer -DomainName "marvelle.local"
+}
 Restart-Computer
