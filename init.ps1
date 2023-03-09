211bbc9f0b9b2f7360b4122215b3918a10e96bdc
Limerio <44976231+Limerio@users.noreply.github.com>
2023-03-09 15:39:04 +0100

    Add Dns Primary Zone in init script

--- init.ps1
+++ init.ps1
@@ -13,7 +13,8 @@ catch {
 if (!$Error) {
 	Add-DhcpServerv4ExclusionRange -ScopeId 192.168.0.0 -StartRange 192.168.0.1 -EndRange 192.168.0.10
 	Set-DhcpServerv4OptionValue -ScopeId 192.168.0.0 -DnsServer 192.168.0.1 -DnsDomain "marvelle.local" -Router 192.168.0.254 
-	Set-DhcpServerv4Scope -ScopeId 192.168.0.0 -Name "projet" -State Active
+	Set-DhcpServerv4Scope -ScopeId 192.168.0.0 -Name "marvelle-scope" -State Active
+	Add-DnsServerPrimaryZone -NetworkId "192.168.0.0/24" -ZoneFile "168.192.marvelle.local" -DynamicUpdate NonsecureAndSecure
 	Write-Host "Le réseau étendu a été créer" -ForegroundColor Green
 }
 
