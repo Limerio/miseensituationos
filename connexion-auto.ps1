1e5579a7de88a569d20b73c383793c702de37bc3
Limerio <44976231+Limerio@users.noreply.github.com>
2023-03-16 14:52:16 +0100

    Update connexion-auto.ps1

--- connexion-auto.ps1
+++ connexion-auto.ps1
@@ -2,7 +2,8 @@ $Error.Clear()
 netsh interface ipv4 set address "ethernet" dhcp
 netsh interface ipv4 set dnsserver "ethernet" dhcp
 Rename-Computer -NewName "CPCO1"
-if(!$Error){
-Add-Computer -DomainName "marvelle.local"
+if (!$Error) {
+  Add-Computer -DomainName "marvelle.local"
+  Restart-Computer
 }
-Restart-Computer
+
